1b4f2032e71980fe87d4c53a9fcd9af99144dbc4
Cynthiamf <c_martiny@outlook.com>
2019-11-08 10:02:19 +0100

    responsive tablet

--- docs/js/index.js
+++ docs/js/index.js
@@ -70,51 +70,77 @@ anchorsLinks.forEach(anchor => {
 });
 
 
-if (window.innerWidth >= 768) {
-  const pageable = new Pageable("main", {
-    interval: 400,
-    delay: 300,
-    events: {
-      touch: false
-    },
+const pageable = new Pageable("main", {
+  interval: 400,
+  delay: 300,
+  events: {
+    touch: false
+  },
 
-    // orientation: "horizontal",
-    // easing: easings.easeOutBounce,
-    onInit: function() {
-      this.pages.forEach((page, i) => {
-        const id = page.id;
-        const text = `${id.charAt(0).toUpperCase()}${id.substr(1)}`;
+  // orientation: "horizontal",
+  // easing: easings.easeOutBounce,
+  onInit: function() {
+    this.pages.forEach((page, i) => {
+      const id = page.id;
+      const text = `${id.charAt(0).toUpperCase()}${id.substr(1)}`;
 
-        // generate top menu
-        const li = document.createElement("li");
-        const a = document.createElement("a");
+      // generate top menu
+      const li = document.createElement("li");
+      const a = document.createElement("a");
 
-        a.href = `#${page.id}`;
+      a.href = `#${page.id}`;
 
-        a.textContent = text.replace("-", " ");
+      a.textContent = text.replace("-", " ");
 
-        if (i === 0) {
-          a.classList.add("active");
-        }
+      if (i === 0) {
+        a.classList.add("active");
+      }
 
-        li.appendChild(a);
-        frag.appendChild(li);
-      });
-    },
-    onBeforeStart: function(x, y) {
-      this.pages.forEach((page, i) => {
-        page.firstElementChild.classList.remove("active");
-      });
-    },
-    onScroll: function(y) {},
-    onFinish: function(data) {
-      this.pages.forEach((page, i) => {
-        page.firstElementChild.classList.toggle("active", i === this.index);
+      li.appendChild(a);
+      frag.appendChild(li);
+    });
+  },
+  onBeforeStart: function(x, y) {
+    this.pages.forEach((page, i) => {
+      page.firstElementChild.classList.remove("active");
+    });
+  },
+  onScroll: function(y) {},
+  onFinish: function(data) {
+    this.pages.forEach((page, i) => {
+      page.firstElementChild.classList.toggle("active", i === this.index);
 
-        anchors.firstElementChild.children[
-          i
-        ].firstElementChild.classList.toggle("active", i === this.index);
-      });
-    }
-  });
+      anchors.firstElementChild.children[
+        i
+      ].firstElementChild.classList.toggle("active", i === this.index);
+    });
+  }
+});
+
+
+if (window.innerWidth >= 767) {
+  pageable.init();
 }
+else {
+  pageable.destroy();
+}
+
+window.addEventListener("orientationchange", function() {
+if (window.innerWidth <= 1024 && (window.orientation === 90 || window.orientation === -90)) {
+  pageable.destroy();
+} else if(window.innerWidth <= 767){
+  pageable.destroy();
+} else {
+  pageable.init();
+}
+}, false);
+
+window.addEventListener("load", function() {
+  if (window.innerWidth <= 1024 && (window.orientation === 90 || window.orientation === -90)) {
+    pageable.destroy();
+  }  else if(window.innerWidth <= 767){
+    pageable.destroy();
+  } else {
+    pageable.init();
+  }
+  }, false);
